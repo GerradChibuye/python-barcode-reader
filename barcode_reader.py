ca3503ff67ac92ccdae93906040af9f1f183631f
GerrardChibs <gerrardchibuye@gmail.com>
2025-08-26 12:59:31 +0200

    Update barcode_reader.py

--- barcode_reader.py
+++ barcode_reader.py
@@ -5,6 +5,4 @@ img = Image.open("barcode.png")
 # Decode
 results = decode(img)
 
-for result in results:
-    print("Barcode Type:", result.type)
-    print("Data:", result.data.decode("utf-8"))
+print(results)
